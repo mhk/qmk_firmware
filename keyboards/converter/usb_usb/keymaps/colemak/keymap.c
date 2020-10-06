c5f2a4b88fed808befadf4d596ab549c8c843c05
Marco Kroll <marco.h.kroll@gmail.com>
2020-10-06 21:17:42 +0200

    version 4

--- keyboards/converter/usb_usb/keymaps/colemak/keymap.c
+++ keyboards/converter/usb_usb/keymaps/colemak/keymap.c
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Tap Dance declarations
 enum {
     TD_LSFT_RSFT,
+    TD_LCTL_RCTL,
     TD_LALT_RALT,
 };
 
@@ -32,9 +33,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_LALT_RALT] = ACTION_TAP_DANCE_DOUBLE(KC_LALT, KC_RALT),
 };
 
-
 #define _COLEMAK  (0)
 #define _PROGKEYS (1)
+#define _QWERTY   (2)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: plain Qwerty without layer switching
      *         ,---------------. ,---------------. ,---------------.
@@ -60,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_JYEN, KC_BSPC,         KC_INS,  KC_HOME, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,    KC_STOP, KC_AGIN,
     KC_TAB,           KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_J,   KC_L,   KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC,          KC_DEL,          KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_MENU, KC_UNDO,
     KC_CAPS,          KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   KC_H,   KC_N,   KC_E,    KC_I,    KC_O   , KC_QUOT,          KC_NUHS, LT(1,KC_ENT),                                  KC_P4,   KC_P5,   KC_P6,   KC_PCMM,    KC_SLCT, KC_COPY,
-    TD(TD_LSFT_RSFT), KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RO,   KC_BSLS,                  KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PEQL,    KC_EXEC, KC_PSTE,
-    TD(TD_LCTL_RCTL), KC_LGUI, LT(1,KC_AT), KC_MHEN, KC_HANJ,   KC_SPC,     KC_HAEN, KC_HENK, KC_KANA, LT(1,KC_EXLM), KC_RGUI, KC_APP,  TD(TD_LALT_RALT),KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT, KC_PENT,    KC_FIND, KC_CUT
+    TD(TD_LSFT_RSFT), KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_K,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          DF(0),   KC_BSLS,                  KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PEQL,    KC_EXEC, KC_PSTE,
+    TD(TD_LCTL_RCTL), KC_LGUI, LT(1,KC_AT), KC_MHEN, KC_HANJ,   KC_SPC,     KC_HAEN, KC_HENK, DF(2),   LT(1,KC_EXLM), KC_RGUI, KC_APP,  TD(TD_LALT_RALT),KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT, KC_PENT,    KC_FIND, KC_CUT
     ),
 
     [_PROGKEYS] = LAYOUT_all(
@@ -72,6 +73,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ______,  KC_DQUO, KC_MINS, KC_EQL,  KC_LPRN, KC_AT,   KC_SLSH, KC_0,    KC_1,    KC_2,    KC_3,    KC_RPRN,          ______,  ______,                              ______,______,______,______,    ______,______,
     ______,  ______,  KC_GRV,  KC_DLR,  KC_PIPE, KC_LBRC, KC_HASH, KC_PERC, KC_MINS, KC_7,    KC_8,    KC_9,             ______,  ______,            ______,           ______,______,______,______,    ______,______,
     ______,  ______,  ______,  ______,  ______,         ______,             ______,  ______,  ______,  ______,  ______,  ______,  ______,     ______,______,______,    ______,       ______,______,    ______,______
+    ),
+
+    [_QWERTY] = LAYOUT_all(
+                      KC_F13,  KC_F14,  KC_F15,  KC_F16, KC_F17, KC_F18, KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,
+    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,               KC_PSCR, KC_SLCK, KC_PAUS,    KC_VOLD, KC_VOLU, KC_MUTE, KC_PWR,     KC_HELP,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_JYEN, KC_BSPC,     KC_INS,  KC_HOME, KC_PGUP,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,    KC_STOP, KC_AGIN,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,     KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,    KC_MENU, KC_UNDO,
+    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_NUHS, KC_ENT,                                    KC_P4,   KC_P5,   KC_P6,   KC_PCMM,    KC_SLCT, KC_COPY,
+    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          DF(0),   KC_RSFT,              KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PEQL,    KC_EXEC, KC_PSTE,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_MHEN, KC_HANJ,         KC_SPC,         KC_HAEN, KC_HENK, DF(2),   KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,     KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT, KC_PENT,    KC_FIND, KC_CUT
     ),
 
 #if 0
