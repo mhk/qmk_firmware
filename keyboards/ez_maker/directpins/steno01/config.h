227636e31a15b2fc2c3fff2e0805e1dfcf374d74
Marco Kroll <marco.h.kroll@gmail.com>
2022-11-10 21:54:34 +0100

    steno01 keyboard layout

---
+++ keyboards/ez_maker/directpins/steno01/config.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2022 Kevin Gee <info@controller.works>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+/* #define DEBOUNCE 5 */
+
+/* #define I2C_DRIVER I2CD1  */
+/* #define I2C1_SCL_PIN GP25 */
+/* #define I2C1_SDA_PIN GP24 */
+
+#define EE_HANDS
+#define MASTER_LEFT
+/* #define MASTER_RIGHT */
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
