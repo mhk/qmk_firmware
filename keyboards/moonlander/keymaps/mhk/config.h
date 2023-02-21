a0c05019d519d960a648244b167c7e56b1701c97
Marco Kroll <marco.h.kroll@gmail.com>
2023-02-21 21:54:10 +0100

    Add mhk layout for moonlander

---
+++ keyboards/moonlander/keymaps/mhk/config.h
@@ -0,0 +1,12 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define FIRMWARE_VERSION u8"daWv6/D50WP"
+#define RGB_MATRIX_STARTUP_SPD 60
+
+#define TAPPING_TERM 200
+#define COMBO_COUNT 1
+#define FORCE_NKRO
+// #define TAPPING_TOGGLE 2
