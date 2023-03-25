b8b70a18b67fa1abf0513bee358525b81d182f6e
Marco Kroll <marco.h.kroll@gmail.com>
2023-03-25 10:49:13 +0100

    Fix bug in gergo keymap

--- keyboards/gboards/gergo/keymaps/mhk/keymap.c
+++ keyboards/gboards/gergo/keymaps/mhk/keymap.c
@@ -110,14 +110,14 @@ qk_tap_dance_action_t tap_dance_actions[] = {
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_COLEMAKDHM] = LAYOUT_gergo(
-    KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                                                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_QUOTE,       TD_PLOVER,
+    KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                                                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_QUOTE,       ON_PLOVER,
     KC_ESCAPE,      KC_A,           KC_R,           KC_S,           KC_T,           KC_G,           KC_LALT,                                        KC_RALT,        KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           KC_SLASH,
     KC_BSPACE,      KC_Z,           KC_X,           KC_C,           KC_D,           KC_V,           KC_NO,          TG(_QWERTY),    TG(_MOVEMENT),  KC_NO,          KC_K,           KC_H,           KC_COMMA,       KC_DOT,         KC_SCOLON,      KC_DELETE,
                                                                     CK_LPROG,       KC_ENTER,       KC_LSHIFT,      TD_LCTCM,       TD_RCTCM,       KC_RSHIFT,      KC_SPACE,       CK_RPROG
     ),
 
 [_QWERTY] = LAYOUT_gergo(
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           TD_PLOVER,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           ON_PLOVER,
     KC_ESCAPE,      KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_LALT,                                        KC_RALT,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_BSPACE,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_NO,          TG(_QWERTY),    TG(_MOVEMENT),  KC_NO,          KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_DELETE,
                                                                     CK_LPROG,       KC_ENTER,       KC_LSHIFT,      TD_LCTCM,       TD_RCTCM,       KC_RSHIFT,      KC_SPACE,       CK_RPROG
